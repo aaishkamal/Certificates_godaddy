29de0e9e443cd75ec7a5d70e5012c870dcdb73f5
Sarim Khan <sarim.szk@gmail.com>
2020-09-28 22:03:49 -0400

    update 3

--- godaddy.py
+++ godaddy.py
@@ -1,13 +1,15 @@
 import requests
 import json
 
-api_key = "apikey"
-secret_key = "secretkey"
+api_key = "3mM44UbC1evZBR_B6j76Te2Z9Dy4r4WJJPXjH"
+secret_key = "4t5hA7yFsrswZQC1RrkdFJ"
 
 headers = {"Authorization" : "sso-key {}:{}".format(api_key, secret_key)}
 
-url = "https://api.godaddy.com/v1/certificates/{certificateId}"
+url = "https://api.ote-godaddy.com/v1/certificates"
 
 certificate = requests.get(url, headers=headers).json()
 
-data = json.load(certificate)
+data = json.dumps(certificate)
+
+print(data)
